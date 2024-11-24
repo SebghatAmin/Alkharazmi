9461673f6da0a033ccfcaa4b88dc7d74257b44ca
Office Laptop <sebghatullah1994@gmail.com>
2024-11-24 13:42:21 +0430

    started

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <header>
+      <div>
+        <h1 className="text-3xl  font-serif underline bg-slate-500">
+          Hello world!
+        </h1>
+      </div>
+    </header>
+  );
+}
