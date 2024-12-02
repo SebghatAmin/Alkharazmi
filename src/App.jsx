1cd63c6712120007cc0e24ed56f696ff762441c2
Sebghatullah Amin <95332899+SebghatAmin@users.noreply.github.com>
2024-12-02 08:47:18 +0430

    updated 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,3 @@
 export default function App() {
-  return (
-    <header className="flex items-center  h-40 w-full bg-gray-900 ">
-      <a href="https://gaaga.wpengine.com/" rel="home">
-        <img
-          className="ml-20 mr-auto"
-          src="./public/al-kharazmi_circle-facebook.png"
-          alt="GaaGa Site"
-          width="64"
-          height="24"
-        />
-      </a>
-      <ul className="flex text-neutral-50 mr-auto bg-slate-600">
-        <li className="p-5 text-4xl font-bold font-sans">HOME</li>
-        <li className="p-5 text-4xl font-bold font-sans">PAGES</li>
-        <li className="p-5 text-4xl font-bold font-sans">PROJECTS</li>
-        <li className="p-5 text-4xl font-bold font-sans">SERVICES</li>
-        <li className="p-5 text-4xl font-bold font-sans">BLOG</li>
-        <li className="p-5 text-4xl font-bold font-sans">CONTACT US</li>
-      </ul>
-    </header>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
