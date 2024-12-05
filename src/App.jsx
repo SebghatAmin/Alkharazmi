0fdb85d572b6639d3026c1df71515eae5b3574e9
Office Laptop <sebghatullah1994@gmail.com>
2024-12-05 16:07:33 +0430

    Hero started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Nav from "./components/Nav";
+import Hero from "./sections/Hero";
 
 export default function App() {
   return (
     <main>
       <Nav />
+      <Hero />
     </main>
 
     // <header />
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,5 @@
+const Hero = () => {
+  return <div className="bg-slate-500 h-[600px]">Hero </div>;
+};
+
+export default Hero;
