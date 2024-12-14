18419bdf4390c980fd78f71af06c0b910d07f550
Office Laptop <sebghatullah1994@gmail.com>
2024-12-14 16:47:40 +0430

    update seven

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,27 +1,37 @@
 const Nav = () => {
   return (
     <header className="fixed w-full top-0">
-      <nav className="flex items-center justify-between  h-40 w-full bg-gray-900 ">
+      <nav className="flex items-center justify-between h-40 w-full bg-gray-900">
         <a href="https://gaaga.wpengine.com/" rel="home">
           <img
             className="ml-20 mr-auto"
             src="/al-kharazmi_circle-facebook.png"
-            alt="GaaGa Site"
+            alt="Al-kharazmi"
             width="64"
             height="24"
           />
         </a>
-        <ul className="flex text-neutral-50 ml-96">
+        <ul className="flex text-neutral-50 ml-96 relative">
           <li className="p-5 text-4xl font-bold font-sans">HOME</li>
-          <li className="p-5 text-4xl font-bold font-sans">PAGES</li>
+          <li className="p-5 text-4xl font-bold font-sans relative group bg-gray-900">
+            PAGES
+            <ul className="absolute left-0 hidden group-hover:flex top-full bg-gray-900 flex-col p-5 text-xl w-60 space-y-2 rounded shadow-lg">
+              <li className=" p-2 rounded">PROCESS</li>
+              <li className=" p-2 rounded">FAQ</li>
+              <li className=" p-2 rounded">PRICING PLAN</li>
+              <li className=" p-2 rounded">OUR TEAM</li>
+              <li className=" p-2 rounded">CAREER</li>
+              <li className=" p-2 rounded">404 PAGE</li>
+            </ul>
+          </li>
           <li className="p-5 text-4xl font-bold font-sans">PROJECTS</li>
           <li className="p-5 text-4xl font-bold font-sans">SERVICES</li>
           <li className="p-5 text-4xl font-bold font-sans">BLOG</li>
           <li className="p-5 text-4xl font-bold font-sans">CONTACT US</li>
         </ul>
-        <div className="flex items-center justify-center border-2 border-red-500 w-72  mr-10 h-28">
+        <div className="flex items-center justify-center border-2 border-red-500 w-72 mr-10 h-28">
           <h1 className="font-sans font-bold text-4xl text-white">
-            Lets start
+            Let's start
           </h1>
         </div>
       </nav>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
-    <div className="bg-slate-500 h-[8000px] w-[2500px] mt-40">
-      <h1 className="text-8xl ml-auto mr-auto  w-fit bg-red-500">
+    <div className="bg-slate-500 h-[8000px] w-[2650px] mt-40 ml-auto mr-auto">
+      <h1 className="text-8xl ml-auto mr-auto  w-fit">
         Hero section is where you put all your content.
       </h1>
     </div>
